a8b4e01298a49a8b3a48cf174527172f02b336f1
Tim Schneeberger <tim.schneeberger@outlook.de>
2025-10-11 04:07:21 +0200

    Migrate WebAPI to WebService

--- src/services/WebService.h
+++ src/services/WebService.h
@@ -6,9 +6,10 @@
 #define WEBINTERFACE_H
 
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <map>
 
 #include "Task.h"
-#include "WebApi.h"
 #include "utils/Macros.h"
 
 class WebService final : public Task {
@@ -19,8 +20,35 @@ protected:
     [[noreturn]] void run() override;
 
 private:
-    WebApi api;
+    AsyncWebServer server;
 
+    struct UploadState {
+        uint8_t* buffer = nullptr;
+        size_t size = 0;
+        size_t offset = 0;
+        uint64_t lastActivityTime = 0;
+    };
+    std::map<String, UploadState> uploadStates;
+
+    void onRootRequest(AsyncWebServerRequest *request);
+    void onSaveRequest(AsyncWebServerRequest *request) const;
+    void onUpdateRequest(AsyncWebServerRequest *request);
+    void onUpdatePostRequest(AsyncWebServerRequest *request);
+    void onUpdatePostUpload(AsyncWebServerRequest *request, const String& filename, size_t index, uint8_t *data, size_t len, bool final);
+    void onDeleteAllRequest(AsyncWebServerRequest *request);
+    void onShowImageRequest(AsyncWebServerRequest *request);
+    void onShowImagePostRequest(AsyncWebServerRequest *request);
+
+    void printImageUploadStates();
+
+    void onShowImagePostUpload(AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data,size_t len, bool final);
+    void onShowMessageRequest(AsyncWebServerRequest *request);
+    void onRebootRequest(AsyncWebServerRequest *request);
+    void onKeepAliveRequest(AsyncWebServerRequest *request);
+    void onDiagRequest(AsyncWebServerRequest *request);
+
+    static String getImagesList();
+    static String getCheckedAttribute(bool isChecked);
 };
 
 #endif // WEBINTERFACE_H
